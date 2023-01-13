b6dff837c77e02ebcc1d18a40c2a8609ddf4ec10
EllieJaybee <muhamadaidilalimi@gmail.com>
2023-01-13 23:15:42 +0800

    Remove banner ew

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,6 +1,6 @@
 import crescent
 from bot.secret import TOKEN
 
-bot = crescent.Bot(TOKEN, intents=crescent.Intents.ALL)
+bot = crescent.Bot(TOKEN, intents=crescent.Intents.ALL, banner=None)
 bot.plugins.load_folder("bot.plugins")
 bot.run()
