1f7eb68f24fe4a7a42eacd5c494e5ef31339f775
EllieJaybee <muhamadaidilalimi@gmail.com>
2023-02-09 23:18:29 +0800

    fuck banners lmao

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,7 +2,7 @@ import crescent
 import hikari
 from bot.secret import TOKEN
 
-client = hikari.GatewayBot(TOKEN)
+client = hikari.GatewayBot(TOKEN, banner=None)
 bot = crescent.Client(client)
 bot.plugins.load_folder("bot.plugins")
 client.run()
