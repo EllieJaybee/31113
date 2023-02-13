0899cfcae7e22d5a84153a3184b27e53ad0f946b
EllieJaybee <muhamadaidilalimi@gmail.com>
2023-02-13 23:21:51 +0800

    Hotfix

--- bot/plugins/context.py
+++ bot/plugins/context.py
@@ -14,6 +14,10 @@ async def sauce(ctx: crescent.Context, message: hikari.Message):
         return await ctx.respond("No attachments to be sauced")
     await ctx.defer()
     sauceclient = SauceNao(api_key=SAUCETOKEN, results_limit=1)
+    for word in message.content.split():
+        if word.startswith("https://"):
+            sourced = await sauceclient.from_url(word)
+            await ctx.respond(sourced[0].url)
     for att in message.attachments:
         sourced = await sauceclient.from_url(att.url)
         await ctx.respond(sourced[0].url)
