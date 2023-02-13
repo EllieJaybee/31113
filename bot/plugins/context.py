9db1dcda2defb271708f294a72de26139c5faf95
EllieJaybee <muhamadaidilalimi@gmail.com>
2023-02-14 00:10:26 +0800

    Pain

--- bot/plugins/context.py
+++ bot/plugins/context.py
@@ -1,6 +1,6 @@
 import crescent
 import hikari
-from pysaucenao import SauceNao
+from pysaucenao import SauceNao, errors
 from typing_extensions import Annotated as atd
 
 from bot.secret import SAUCETOKEN
@@ -10,14 +10,21 @@ plugin = crescent.Plugin()
 @plugin.include
 @crescent.message_command(name="Find Sauce")
 async def sauce(ctx: crescent.Context, message: hikari.Message):
-    if not message.attachments:
-        return await ctx.respond("No attachments to be sauced")
-    await ctx.defer()
+    await ctx.defer(ephemeral=True)
     sauceclient = SauceNao(api_key=SAUCETOKEN, results_limit=1)
-    for word in message.content.split():
-        if word.startswith("https://"):
-            sourced = await sauceclient.from_url(word)
+    if message.attachments:
+        for att in message.attachments:
+            sourced = await sauceclient.from_url(att.url)
             await ctx.respond(sourced[0].url)
-    for att in message.attachments:
-        sourced = await sauceclient.from_url(att.url)
-        await ctx.respond(sourced[0].url)
+    elif message.content:
+        if "https://" not in message.content:
+            return await ctx.respond("Nothing to be sauced")
+        for word in message.content.split():
+            if word.startswith("https://"):
+                try:
+                    sourced = await sauceclient.from_url(word)
+                    await ctx.respond(sourced[0].url)
+                except errors.InvalidImageException:
+                    await ctx.respond("Invalid url")
+    else:
+        await ctx.respond("Nothing to be sauced")
