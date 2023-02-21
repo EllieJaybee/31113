9ce2befcc9ce323e4a7076ab631d780051fcaaf7
EllieJaybee <muhamadaidilalimi@gmail.com>
2023-02-21 22:06:56 +0800

    Button to repeat reddit commands

--- bot/plugins/reddit.py
+++ bot/plugins/reddit.py
@@ -1,4 +1,6 @@
 import crescent
+import hikari
+import miru
 from typing_extensions import Annotated as atd
 
 import aiohttp
@@ -8,6 +10,10 @@ import random
 plugin = crescent.Plugin()
 subsort = ["hot", "new", "rising", "top"]
 timelist = ["hour", "day", "week", "month", "year", "all"]
+
+@plugin.load_hook
+def load():
+    miru.install(plugin.app)
 
 async def request(ctx: crescent.Context, endpoint: str):
     await ctx.defer()
@@ -26,35 +32,51 @@ async def gallery(ctx: crescent.Context, gallery_data: dict):
         _format = p['m'].split("/")[-1]
         await ctx.respond(f"https://i.reddit.com/{_id}.{_format}")
 
-async def reddit(ctx: crescent.Context, subreddit: str, offset: int):
-    if not ctx.channel.is_nsfw:
-        return await ctx.respond("horny 🫵", ephemeral=True)
-    d = await request(ctx, f"r/{subreddit}/{random.choice(subsort)}.json")
-    postlist = d['data']['children'][offset:]
-    post = random.choice(postlist)
-    try:
-        if "reddit.com/gallery/" in post['data']['url_overridden_by_dest']:
-            return await gallery(ctx, post)
-        await ctx.respond(post['data']['url_overridden_by_dest'])
-    except KeyError:
+class MoreButton(miru.Button):
+    def __init__(self, sub: str):
+        super().__init__(style=hikari.ButtonStyle.SUCCESS, label="More🔄️")
+        self.sub = sub
+        reddict = {"Femboys": 1, "traphentai": 2, "hentai": 3}
+        self.offset = reddict[sub] if sub in reddict else 0
+
+    async def callback(self, ctx: miru.ViewContext):
+        await MoreButton.reddit(ctx, self.sub, self.offset, False)
+        self.view.stop()
+
+    @classmethod
+    async def reddit(self, ctx: crescent.Context, subreddit: str, offset: int, _new: bool = True):
+        if _new and not ctx.channel.is_nsfw:
+            return await ctx.respond("horny 🫵", flags=hikari.MessageFlag.EPHEMERAL)
+        d = await request(ctx, f"r/{subreddit}/{random.choice(subsort)}.json")
+        postlist = d['data']['children'][offset:]
+        post = random.choice(postlist)
+        try:
+            if "reddit.com/gallery/" in post['data']['url_overridden_by_dest']:
+                return await gallery(ctx, post)
+            view = miru.View()
+            view.add_item(MoreButton(subreddit))
+            mes = await ctx.respond(post['data']['url_overridden_by_dest'], components=view)
+            await view.start(mes)
+            await view.wait()
+        except KeyError:
             await ctx.respond("The post queried is not a media post")
 
 @plugin.include
 @crescent.command(name="reddit", description="Fetches hot reddit stuff")
 async def _reddit(ctx: crescent.Context, subreddit: atd[str, "subreddit you wanna fetch"]):
-    await reddit(ctx, subreddit, 0)
+    await MoreButton.reddit(ctx, subreddit, 0)
 
 @plugin.include
 @crescent.command(description="Fetches a random hot reddit femboy post")
 async def femboy(ctx: crescent.Context):
-    await reddit(ctx, "Femboys", 1)
+    await MoreButton.reddit(ctx, "Femboys", 1)
 
 @plugin.include
 @crescent.command(description="Fetches a random hot reddit trap hentai post")
 async def trap(ctx: crescent.Context):
-    await reddit(ctx, "traphentai", 2)
+    await MoreButton.reddit(ctx, "traphentai", 2)
 
 @plugin.include
 @crescent.command(description="Fetches a random hot reddit hentai post")
 async def hentai(ctx: crescent.Context):
-    await reddit(ctx, "hentai", 0)
+    await MoreButton.reddit(ctx, "hentai", 0)
