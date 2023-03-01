42f4a91afa27eedfd0077722fdd5d3af92793e40
EllieJaybee <muhamadaidilalimi@gmail.com>
2023-03-01 13:00:30 +0800

    privileged commands

---
+++ bot/plugins/privileges/femboy.py
@@ -0,0 +1,18 @@
+import crescent
+import hikari
+from typing_extensions import Annotated as atd
+
+plugin = crescent.Plugin()
+
+async def is_femboy(ctx: crescent.Context):
+    if 965287802935853127 not in ctx.member.role_ids and ctx.member.id != 845942758421823488:
+        await ctx.respond("You have no such permission")
+        return crescent.HookResult(exit=True)
+    return crescent.HookResult()
+
+@plugin.include
+@crescent.hook(is_femboy)
+@crescent.command(description="Grant Member role")
+async def member(ctx: crescent.Context, memb: atd[hikari.User, "Person to be a member"]):
+    await memb.add_role(938700639771439157)
+    await ctx.respond(f"Given {memb.display_name} Member", mentions_everyone=False, role_mentions=False, user_mentions=False)
