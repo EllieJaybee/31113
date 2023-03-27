a9b9ac4c5664bf9f56c13f786c2c31756204a6e4
EllieJaybee <muhamadaidilalimi@gmail.com>
2023-03-27 16:24:32 +0800

    Prevent being run when imported

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,8 +3,12 @@ import hikari
 import miru
 from bot.secret import TOKEN
 
-client = hikari.GatewayBot(TOKEN, banner="bot")
-bot = crescent.Client(client)
-miru.install(client)
-bot.plugins.load_folder("bot.plugins")
-client.run()
+def main():
+    client = hikari.GatewayBot(TOKEN, banner="bot")
+    bot = crescent.Client(client)
+    miru.install(client)
+    bot.plugins.load_folder("bot.plugins")
+    client.run()
+
+if __name__ == "__main__":
+    main()
