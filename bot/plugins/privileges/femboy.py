3ccc3f42d9ff4676ea2e4c64b8119cd0aeeb50c6
EllieJaybee <muhamadaidilalimi@gmail.com>
2023-04-13 01:45:33 +0800

    hotter fix

--- bot/plugins/privileges/femboy.py
+++ bot/plugins/privileges/femboy.py
@@ -12,7 +12,7 @@ async def is_femboy(ctx: crescent.Context):
 
 async def give_member(ctx: crescent.Context, member: hikari.Member):
     if 938700639771439157 in member.role_ids:
-        return await ctx.respond(f"💀 {member.display_name} already has Member")
+        return await ctx.respond(f"💀 {member.display_name} already has Member", mentions_everyone=False, role_mentions=False, user_mentions=False)
     await member.add_role(938700639771439157)
     await ctx.respond(f"🍆 Given {member.display_name} Member", mentions_everyone=False, role_mentions=False, user_mentions=False)
 
@@ -22,8 +22,8 @@ async def give_member(ctx: crescent.Context, member: hikari.Member):
 async def member_slash(ctx: crescent.Context, member: atd[hikari.User, "Person to be a member"]):
     await give_member(ctx, member)
 
-@plugin.include
-@crescent.hook(is_femboy)
-@crescent.message_command(name="Grant Member role")
-async def member_message(ctx: crescent.Context, message: hikari.Message):
-    await give_member(ctx, message.author)
+# @plugin.include
+# @crescent.hook(is_femboy)
+# @crescent.message_command(name="Grant Member role")
+# async def member_message(ctx: crescent.Context, message: hikari.Message):
+#     await give_member(ctx, message.member)
