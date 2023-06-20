b9f0f5ca6ce4e181a104045d226818970ba78d12
EllieJaybee <muhamadaidilalimi@gmail.com>
2023-06-20 13:58:53 +0800

    Security change

--- bot/plugins/rng.py
+++ bot/plugins/rng.py
@@ -13,4 +13,4 @@ async def rng(ctx: crescent.Context, min: atd[int, "Lowest number"], max: atd[in
 @crescent.command(description="Pick randomly from a list of choices")
 async def pick(ctx: crescent.Context, choices: atd[str, "Choices separated by commas"]):
     choice_list = choices.split(",")
-    await ctx.respond(random.choice(choice_list))
+    await ctx.respond(random.choice(choice_list), mentions_everyone=False, role_mentions=False, user_mentions=False)
