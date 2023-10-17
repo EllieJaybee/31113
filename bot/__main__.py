af09fed501c5a54f7a4d49ae2f86fe7ef3729f6e
EllieJaybee <muhamadaidilalimi@gmail.com>
2023-10-17 21:28:24 +0800

    Fixed lol

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,7 +4,9 @@ import miru
 from bot.secret import TOKEN
 
 def main():
-    bot = hikari.GatewayBot(TOKEN, banner="bot")
+
+    intents = hikari.Intents.ALL
+    bot = hikari.GatewayBot(TOKEN, banner="bot", intents=intents)
     client = crescent.Client(bot)
     miru.install(bot)
     client.plugins.load_folder("bot.plugins")
--- bot/plugins/rng.py
+++ bot/plugins/rng.py
@@ -5,6 +5,12 @@ from typing_extensions import Annotated as atd
 plugin = crescent.Plugin()
 
 @plugin.include
+@crescent.command(description="Pick a random member")
+async def pickmember(ctx: crescent.Context):
+    membersview = plugin.app.cache.get_members_view_for_guild(ctx.guild_id)
+    await ctx.respond(random.choice(list(membersview.values())), mentions_everyone=False, role_mentions=False, user_mentions=False)
+
+@plugin.include
 @crescent.command(description="Generates a random number in a range")
 async def rng(ctx: crescent.Context, min: atd[int, "Lowest number"], max: atd[int, "Highest number"]):
 	await ctx.respond(random.randint(min, max))
@@ -14,8 +20,3 @@ async def rng(ctx: crescent.Context, min: atd[int, "Lowest number"], max: atd[in
 async def pick(ctx: crescent.Context, choices: atd[str, "Choices separated by commas"]):
     choice_list = choices.split(",")
     await ctx.respond(random.choice(choice_list), mentions_everyone=False, role_mentions=False, user_mentions=False)
-
-@plugin.include
-@crescent.command(description="Pick a random member")
-async def pick_member(ctx: crescent.Context):
-      await ctx.respond(random.choice(ctx.guild.get_members()), mentions_everyone=False, role_mentions=False, user_mentions=False)
