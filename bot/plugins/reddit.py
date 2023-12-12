6e49e953f9caa843a6c510dc68b187e82cd4777e
EllieJaybee <muhamadaidilalimi@gmail.com>
2023-12-13 04:21:51 +0800

    exp

--- bot/plugins/reddit.py
+++ bot/plugins/reddit.py
@@ -62,7 +62,7 @@ class MoreButton(miru.Button):
             #    return await gallery(ctx, post)
             view = miru.View(timeout=None)
             view.add_item(MoreButton(subreddit))
-            message = await ctx.respond(str(vars(post))[:500], components=view)
+            message = await ctx.respond(post.url_overridden_by_dest, components=view)
             await view.start(message)
             await view.wait()
         except KeyError:
