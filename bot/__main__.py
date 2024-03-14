ff5c79b25aa2ac2d2fda2477107807750f7d7ccc
EllieJaybee <muhamadaidilalimi@gmail.com>
2024-03-14 08:36:55 +0800

    Update to miru v4

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,15 +1,20 @@
+from dataclasses import dataclass
 import crescent
 import hikari
 import miru
 from bot.secret import TOKEN
 
+@dataclass
+class Model:
+    miru: miru.Client
+
 def main():
 
     intents = hikari.Intents.ALL
     bot = hikari.GatewayBot(TOKEN, banner="bot", intents=intents)
-    client = crescent.Client(bot)
-    miru.install(bot)
-    client.plugins.load_folder("bot.plugins")
+    miru_client = miru.Client(bot)
+    crescent_client = crescent.Client(bot, Model(miru_client))
+    crescent_client.plugins.load_folder("bot.plugins")
     bot.run()
 
 if __name__ == "__main__":
--- bot/plugins/reddit.py
+++ bot/plugins/reddit.py
@@ -4,70 +4,75 @@ import miru
 from typing_extensions import Annotated as atd
 
 import asyncpraw
-from asyncpraw.models import Subreddit, Submission
+from asyncpraw.models import Subreddit
 
 from bot.secret import REDID, REDSECRET
+from bot.__main__ import Model
 
-plugin = crescent.Plugin()
+Plugin = crescent.Plugin[hikari.GatewayBot, Model]
+plugin = Plugin()
 
 class RedditView(miru.View):
 
+    def __init__(self, subreddit):
+        self.subreddit: str = subreddit
+        super().__init__(timeout=600)
+
+    @miru.button(label="More🔄️", style=hikari.ButtonStyle.SUCCESS)
+    async def more(self, ctx: miru.ViewContext, button: miru.Button):
+        button.custom_id = self.subreddit
+        await ctx.edit_response(components=None)
+        await reddit(ctx, self.subreddit)
+
     async def on_timeout(self):
         await self.message.edit(components=None)
-        await MoreButton.preddit.close()
 
-class MoreButton(miru.Button):
-    def __init__(self, subreddit: str):
-        super().__init__(style=hikari.ButtonStyle.SUCCESS, label="More🔄️")
-        self.sub = subreddit
-
-    async def callback(self, ctx: miru.ViewContext):
-        await ctx.edit_response(components=None)
-        await MoreButton.reddit(ctx, self.sub, False)
-        self.view.stop()
-
-    @classmethod
-    async def reddit(cls, ctx: crescent.Context, subreddit: str, new_: bool = True):
-        if new_:
-            await ctx.defer()
-            cls.preddit = asyncpraw.Reddit(
-            client_id=REDID,
-            client_secret=REDSECRET,
-            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-            )
-        sub: Subreddit = await cls.preddit.subreddit(subreddit, fetch=True)
-        if sub.over18:
-            if isinstance(ctx, crescent.Context) and not ctx.channel.is_nsfw:
-                await cls.preddit.close()
-                return await ctx.respond("horny 🫵", flags=hikari.MessageFlag.EPHEMERAL)
-        post = await sub.random()
-        view = RedditView(timeout=600)
-        view.add_item(MoreButton(subreddit))
-        message = await ctx.respond(f"[⠀](https://rxddit.com{post.permalink})", components=view)
-        await view.start(message)
-        await view.wait()
+async def reddit(ctx: crescent.Context | miru.ViewContext, subreddit: str):
+    if isinstance(ctx, crescent.Context):
+        await ctx.defer()
+    preddit = asyncpraw.Reddit(
+        client_id = REDID,
+        client_secret = REDSECRET,
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+    )
+    sub: Subreddit = await preddit.subreddit(subreddit, fetch=True)
+    if sub.over18:
+        if isinstance(ctx, crescent.Context) and not ctx.channel.is_nsfw:
+            await preddit.close()
+            return await ctx.respond("horny 🫵", ephemeral=True)
+    post = await sub.random()
+    view = RedditView(subreddit=subreddit)
+    respkwargs = {"components": view}
+    if isinstance(ctx, crescent.Context):
+        respkwargs["ensure_message"] = True
+    message = await ctx.respond(f"[⠀](https://rxddit.com{post.permalink})", **respkwargs)
+    await preddit.close()
+    if isinstance(ctx, miru.ViewContext):
+        message = await message.retrieve_message()
+    plugin.model.miru.start_view(view, bind_to=message)
+    await view.wait()
 
 @plugin.include
 @crescent.command(name="reddit", description="Fetches hot reddit stuff")
 async def reddit_(ctx: crescent.Context, subreddit: atd[str, "subreddit you wanna fetch"]):
-    await MoreButton.reddit(ctx, subreddit)
+    await reddit(ctx, subreddit)
 
 @plugin.include
 @crescent.command(description="Fetches a random hot reddit femboy post")
 async def femboy(ctx: crescent.Context):
-    await MoreButton.reddit(ctx, "Femboys")
+    await reddit(ctx, "Femboys")
 
 @plugin.include
 @crescent.command(description="Fetches a random hot reddit trap hentai post")
 async def trap(ctx: crescent.Context):
-    await MoreButton.reddit(ctx, "traphentai")
+    await reddit(ctx, "traphentai")
 
 @plugin.include
 @crescent.command(description="Fetches a random hot reddit hentai post")
 async def hentai(ctx: crescent.Context):
-    await MoreButton.reddit(ctx, "hentai")
+    await reddit(ctx, "hentai")
 
 @plugin.include
 @crescent.command(description="Fetches cat pics")
 async def meow(ctx: crescent.Context):
-    await MoreButton.reddit(ctx, "cats")
+    await reddit(ctx, "cats")
