ec3dd79e63418d460c3d9ea0f3b1785d3f799951
EllieJaybee <muhamadaidilalimi@gmail.com>
2024-07-05 04:48:19 +0800

    Fix mute command

--- bot/plugins/privileges/mod.py
+++ bot/plugins/privileges/mod.py
@@ -13,11 +13,18 @@ plugin = crescent.Plugin()
 async def mute_1984(
     ctx: crescent.Context, member: atd[hikari.User, "Member to silence"]
 ):
+    log_channel: hikari.TextableChannel = ctx.guild.get_channel(939418125894553611)
+    member: hikari.Member = member
     await member.edit(mute=True, deaf=True)
+    roles_removed = []
+    for role_id in member.role_ids:
+        roles_removed.append(ctx.guild.get_role(role_id).name)
+        await member.remove_role(role_id)
+    await log_channel.send(
+        f"Removed `{', '.join(roles_removed)}` roles from {member.username}"
+    )
+
     await member.add_role(939351651217735760)
-    await member.remove_role(938700639771439157)
-    await member.remove_role(965287802935853127)
-    await member.remove_role(939494839077204018)
     await ctx.respond(f"✅ Locked {member.display_name} up", ephemeral=True)
 
 
@@ -25,11 +32,12 @@ async def mute_1984(
 @crescent.hook(is_mod)
 @crescent.command(description="Release prisoner", guild=938699961112096768)
 async def free(ctx: crescent.Context, member: atd[hikari.User, "Prisoner to free"]):
+    member: hikari.Member = member
     if 939351651217735760 not in member.role_ids:
         return await ctx.respond("They're not gulaged", ephemeral=True)
     await member.edit(mute=False, deaf=False)
     await member.remove_role(939351651217735760)
     await ctx.respond(
-        f"✅ Freed {member.display_name} (Member, Big boy and Femboy role needs to be manually given)",
+        f"✅ Freed {member.display_name} (Roles need to be manually given)",
         ephemeral=True,
     )
