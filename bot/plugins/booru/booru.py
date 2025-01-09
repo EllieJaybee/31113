4d9fe24ebbe1ff07a80dc28abf2d8c7c5708b48e
EllieJaybee <muhamadaidilalimi@gmail.com>
2025-01-09 13:17:55 +0800

    Add booru site option to BooruCommand for flexible searches

--- bot/plugins/booru/booru.py
+++ bot/plugins/booru/booru.py
@@ -23,6 +23,17 @@ class BooruCommand:
         default="safe",
         choices=[("safe", "safe"), ("explicit", "explicit")],
     )
+    booru = crescent.option(
+        str,
+        "The booru site to search on.",
+        default="gb",
+        choices=[
+            ("Gelbooru", "gb"),
+            ("E621", "e6"),
+            ("E926", "e9"),
+            ("Danbooru", "db"),
+        ],
+    )
 
     async def callback(self, ctx: crescent.Context):
         await ctx.defer()
@@ -37,16 +48,17 @@ class BooruCommand:
                 ) + " "
             except IndexError:
                 return await ctx.respond(f'Tag "{tag}" not found.')
-        finaltags += f"rating:{self.rating} sort:random "
+        finaltags += "sort:random " if self.booru == "gb" else "order:random"
         if self.rating == "explicit":
             finaltags = finaltags.replace("loli", "")
             finaltags = finaltags.replace("shota", "")
             finaltags += "-loli -shota"
         try:
             post = await cunnypy.search(
-                "gelbooru",
+                self.booru,
                 finaltags,
                 limit=1,
+                rating=self.rating,
                 credentials={
                     "user_id": plugin.model.secret.GELBOORU_ID,
                     "api_key": plugin.model.secret.GELBOORU_KEY,
